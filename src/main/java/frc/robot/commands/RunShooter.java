a582ebf4e83fc9150ab73cf040af0ccd2801d145
pryme-svg <edoc.www@gmail.com>
2021-12-30 14:49:35 -0800

    call SetPower in execute

--- src/main/java/frc/robot/commands/RunShooter.java
+++ src/main/java/frc/robot/commands/RunShooter.java
@@ -29,7 +29,9 @@ public class RunShooter extends CommandBase {
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    this.shooterSub.setPower(0.5);
+  }
 
   // Called once the command ends or is interrupted.
   @Override
