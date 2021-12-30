92d62a8c05ceaeb25b0e61cc74819b648f3d9f89
pryme-svg <edoc.www@gmail.com>
2021-12-30 15:10:37 -0800

    bind key to RunShooter

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,10 +7,12 @@ package frc.robot;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.ExampleCommand;
+import frc.robot.commands.RunShooter;
 import frc.robot.commands.ShooterAutonomous;
 import frc.robot.subsystems.ExampleSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -38,7 +40,10 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
-  private void configureButtonBindings() {}
+  private void configureButtonBindings() {
+    new JoystickButton(xbox, PaddedXbox.F310Map.kGamepadButtonA.value)
+    .whileHeld(new RunShooter(shooter));
+  }
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
